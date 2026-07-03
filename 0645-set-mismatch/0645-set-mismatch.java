bdcacd7731bac042480c4883889ad0653f90800d
Pranay Yadav <160768688+pranayyadav998@users.noreply.github.com>
2026-07-03 23:35:42 +0530

    Time: 21 ms (10.48%), Space: 47.7 MB (50.7%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,20 +2,20 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums){
+        for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         int duplicate = -1;
         int missing = -1;
 
-        for(int i=1;i<=nums.length;i++){
-            if(map.getOrDefault(i,0) == 2){
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.getOrDefault(i, 0) == 2) {
                 duplicate = i;
             }
-            if(map.getOrDefault(i,0) == 0){
+            if (map.getOrDefault(i, 0) == 0) {
                 missing = i;
             }
         }
-        return new int[]{duplicate, missing};
+        return new int[] { duplicate, missing };
     }
 }
